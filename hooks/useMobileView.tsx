cb2a9e905fc64381022b41b1b68de5a9dc4eb9a4
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-05-23 00:38:32 +0700

    return new value

--- hooks/useMobileView.tsx
+++ hooks/useMobileView.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 
 const useMobileView = () => {
-    const [width, setWidth] = useState<number>(typeof window !== 'undefined'? window.innerWidth : 1366);
+    const [width, setWidth] = useState<number>(typeof window !== 'undefined' ? window.innerWidth : 1366);
 
     function handleWindowSizeChange() {
         setWidth(window?.innerWidth);
     }
-    
+
     useEffect(() => {
         window.addEventListener('resize', handleWindowSizeChange);
         return () => {
@@ -14,7 +14,10 @@ const useMobileView = () => {
         }
     }, []);
 
-    return width <= 768;
+    return { 
+        isMobile: width <= 768, 
+        windowWidth: width 
+    };
 }
 
 export default useMobileView;
